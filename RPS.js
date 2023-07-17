9a6eabb2566d9267fb43c85f312b7c4340303116
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-17 03:12:04 +0300

    Added playRound() logic

--- RPS.js
+++ RPS.js
@@ -6,6 +6,47 @@ function getComputerChoice() {
     return computerChoise;
 }
 
+function playRound(computerChoise, playerChoise) {
+    if (playerChoise === computerChoise){
+        return console.log("Draw!")
+    }
+
+    if (playerChoise === "paper"){
+        if (computerChoise === "rock"){
+            return console.log("You win! "+playerChoise+ " beats " +computerChoise)
+            }
+        if (computerChoise === "scissors"){
+            return console.log("You lose! "+computerChoise+ " beats " +playerChoise)
+            }
+    }
+
+    if (playerChoise === "rock"){
+        if (computerChoise === "scissors"){
+            return console.log("You win! "+playerChoise+ " beats " +computerChoise)
+            }
+        if (computerChoise === "scissors"){
+            return console.log("You lose! "+ computerChoise + " beats " +playerChoise)
+            }
+    }
+
+    if (playerChoise === "scissors"){
+        if (computerChoise === "paper"){
+            return console.log("You win! "+playerChoise+ " beats " +computerChoise)
+            }
+        if (computerChoise === "rock"){
+            return console.log("You lose! "+ computerChoise + " beats " +playerChoise)
+            }
+    }
+
+}
+
+
+
+// function getePlayerChoice(playerChoise) {
+//     let playerSelection;
+    
+//     return playerSelection;
+// }
 
 getComputerChoice();
 
