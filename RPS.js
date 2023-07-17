8458a99f8149e004a33e57452eb4f0821a6779a8
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-17 03:38:59 +0300

    Added game() function - Plays game 5 times against the player and keeps score. To Be Implemented: announce winner

--- RPS.js
+++ RPS.js
@@ -8,46 +8,80 @@ function getComputerChoice() {
 
 function playRound(computerChoise, playerChoise) {
     let winner = "";
+    let player_or_computer = 0;
     if (playerChoise === computerChoise){
         winner = "Draw!"
+        player_or_computer = 3;
     }
 
     if (playerChoise === "paper"){
         if (computerChoise === "rock"){
             winner = ("You win! "+ playerChoise + " beats " + computerChoise)
+            player_or_computer = 1;
             }
         if (computerChoise === "scissors"){
             winner = ("You lose! "+ computerChoise + " beats " + playerChoise)
+            player_or_computer = 2;
             }
     }
 
     if (playerChoise === "rock"){
         if (computerChoise === "scissors"){
             winner = ("You win! "+ playerChoise + " beats " + computerChoise)
+            player_or_computer = 1;
             }
         if (computerChoise === "paper"){
             winner = ("You lose! "+ computerChoise + " beats " + playerChoise)
+            player_or_computer = 2;
             }
     }
 
     if (playerChoise === "scissors"){
         if (computerChoise === "paper"){
             winner = ("You win! "+ playerChoise + " beats " + computerChoise)
+            player_or_computer = 1;
             }
         if (computerChoise === "rock"){
             winner = ("You lose! "+ computerChoise + " beats " + playerChoise)
+            player_or_computer = 2;
             }
     }
 
-    return winner;
+    return [winner, player_or_computer];
 }
 
 const playerSelection = "paper";
-const computerSelection = getComputerChoice();
+//const computerSelection = getComputerChoice();
 
-console.log("Player chooses: " + playerSelection)
+//console.log("Player chooses: " + playerSelection)
+//console.log(playRound(computerSelection, playerSelection));
 
-console.log(playRound(computerSelection, playerSelection));
+// Play the game 5 times and keep count
+function game() {
+    let computerWins = 0;
+    let playerWins = 0;
+    let draws = 0;
+    let winArray = [];
+
+    for (let i = 0; i < 5; i++) {
+
+        winArray = playRound(getComputerChoice(), playerSelection)
+        console.log(winArray[0])
+        if (winArray[1] === 2 && winArray[1] != null) { computerWins++; }
+        if (winArray[1] === 1 && winArray[1] != null) { playerWins++; }
+        if (winArray[1] === 3 && winArray[1] != null) { draws++; }
+
+    }
+
+    console.log("Player wins: " + playerWins)
+    console.log("Computer wins: " + computerWins)
+    console.log("Draws:" + draws)
+
+    console.log("Total played games: " + (draws+playerWins+computerWins))
+
+}
+
+game()
 
 // function getePlayerChoice(playerChoise) {
 //     let playerSelection;
